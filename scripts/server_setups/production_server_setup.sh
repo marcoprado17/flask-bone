787c2d25a55acd45fdf70f303d189a4e36a91df3
marcoprado17 <marco.pdsv@gmail.com>
2016-12-05 15:35:10 -0200

    Adding basic server setup scripts

---
+++ scripts/server_setups/production_server_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SHARED_SERVER_SETUP_FULL_PATH=/vagrant/scripts/server_setups/shared_server_setup.sh
+
+chmod +x $SHARED_SERVER_SETUP_FULL_PATH
+(exec "$SHARED_SERVER_SETUP_FULL_PATH")
+chmod -x $SHARED_SERVER_SETUP_FULL_PATH
---
+++ scripts/server_setups/shared_server_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update;
+sudo apt-get install -y python-pip;
+sudo apt-get install -y python-dev;
+
+sudo pip install Flask;
