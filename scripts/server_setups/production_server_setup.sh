2a4631d5b97790cd77dfc918246ce6cbeedf2afb
marcoprado17 <marco.pdsv@gmail.com>
2016-12-05 19:21:59 -0200

    Renaming shared_server_setup.sh to shared.sh

--- scripts/server_setups/production_server_setup.sh
+++ scripts/server_setups/production_server_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SHARED_SERVER_SETUP_FULL_PATH=/vagrant/scripts/server_setups/shared_server_setup.sh
+SHARED_SERVER_SETUP_FULL_PATH=/vagrant/scripts/server_setups/shared.sh
 
 chmod +x $SHARED_SERVER_SETUP_FULL_PATH
 (exec "$SHARED_SERVER_SETUP_FULL_PATH")
