da5e828da67b93218b428773e3386ac33e19b014
marcoprado17 <marco.pdsv@gmail.com>
2016-12-05 23:00:42 -0200

    Adding configs

---
+++ configs/instance/instance_flask_app_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DEBUG = True
+
+# EXPLAIN_TEMPLATE_LOADING = True
--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 
 from blueprints.home.views import home_blueprint
+from configs import default_flask_app_config
+from configs.instance import instance_flask_app_config
 
 
 app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_object(default_flask_app_config)
+app.config.from_object(instance_flask_app_config)
 
 app.register_blueprint(home_blueprint)
