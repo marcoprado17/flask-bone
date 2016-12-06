00b3f488fd9ab8622f078034497b00e13f57db6e
marcoprado17 <marco.pdsv@gmail.com>
2016-12-06 19:26:37 -0200

    Adding home blueprint module from script

---
+++ src/blueprints/home/data_provider.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class HomeDataProvider:
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        return {
+
+        }
+
+
+home_data_provider = HomeDataProvider()
---
+++ src/blueprints/home/views.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Blueprint, render_template
+
+home_blueprint = Blueprint("home", __name__, static_folder="static", template_folder="templates")
+
+
+@home_blueprint.route("route-suffix")
+def home():
+    return render_template("home/home.html")
