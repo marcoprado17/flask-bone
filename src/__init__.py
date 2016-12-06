f9eee1f9dccc113862f6630e92ad0d62072535dc
marcoprado17 <marco.pdsv@gmail.com>
2016-12-05 23:20:45 -0200

    Redirecting empty url ('/') to home route

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, redirect
 
 from blueprints.home.views import home_blueprint
 from configs import default_flask_app_config
@@ -13,4 +13,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(default_flask_app_config)
 app.config.from_object(instance_flask_app_config)
 
+@app.route("/")
+def home_redirect():
+    return redirect("home")
 app.register_blueprint(home_blueprint, url_prefix="/home")
