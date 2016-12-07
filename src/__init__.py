1f0d2d4cbc394ac18029ef1b487b7b1991cacbe5
marcoprado17 <marco.pdsv@gmail.com>
2016-12-07 19:24:27 -0200

    Implementing bootstrap modal and button in home | Jquery module missing

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask, redirect
 
 from blueprints.home.views import home_blueprint
 from blueprints.shared.views import shared_blueprint
+from blueprints.bootstrap.views import bootstrap_blueprint
 
 from configs import default_flask_app_config
 from configs.instance import instance_flask_app_config
@@ -20,3 +21,4 @@ def home_redirect():
     return redirect("home")
 app.register_blueprint(home_blueprint, url_prefix="/home")
 app.register_blueprint(shared_blueprint, url_prefix="/shared")
+app.register_blueprint(bootstrap_blueprint, url_prefix="/bootstrap")
