a98b46af8f0107f8f018d774a1101f5808436b54
marcoprado17 <marco.pdsv@gmail.com>
2016-12-16 15:31:00 -0200

    Forgot to send in the last commit

---
+++ src/flask_bombril/jinja/filters/filters.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ======================================================================================================================
+# The MIT License (MIT)
+# ======================================================================================================================
+# Copyright (c) 2016 [Marco Aurélio Prado - marco.pdsv@gmail.com]
+# ======================================================================================================================
+
+from jinja2.runtime import Undefined
+
+
+def assert_defined(value):
+    assert not isinstance(value, Undefined)
+    return value
+
+
+def assert_callable(value):
+    assert callable(value)
+    return value
+
+
+def call(func, *args, **kwargs):
+    return func(*args, **kwargs)
+
+
+def if_filter(value, condition, else_value):
+    if condition:
+        return value
+    return else_value
